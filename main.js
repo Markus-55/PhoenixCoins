4da8e4893e49e7473c6e06e9754e3dc024dc335b
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2020-10-12 07:13:20 +0200

    Add heading, change margin and fetch information from url

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+let BASE_URL = "https://api.coingecko.com/api/v3";
+let PRICE = "/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
+
+let url = BASE_URL + PRICE;
+
 $(document).ready(function() {
+
+  fetch(url)
+  .then(res => {
+
+    res.json().then(data => {
+      $("#price").html(data)
+      console.log(data);
+    })
+})
 
 });
