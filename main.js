5a9a6b1091ad23340c402e79c96bb1625a395288
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2020-10-16 13:59:02 +0200

    Create new table row and new table data into html with js file

--- main.js
+++ main.js
@@ -13,46 +13,71 @@ $(document).ready(function() {
       // Then make a function with the data as an argument
       res.json().then(data => {
 
-        // The value of index is = 0
-        var index = 0;
-        // The value of rank is = to the data index market cap rank
-        var rank = data[index].market_cap_rank;
-        // // The value of name is = to the data index name
-        var name = data[index].name;
-        // The value of mark cap is = to the data index market cap
-        var market_cap = data[index].market_cap;
-        // The value of price is = to the data index current price
-        var price = data[index].current_price;
-        // The value of volume 24 is = to the data index market total volume
-        var volume_24 = data[index].total_volume;
-        // The value of circulating supply is = to the data index circulating supply
-        var circulating_supply = data[index].circulating_supply;
-        // The value of change 24 is = to the data index price change 24h
-        var change_24 = data[index].price_change_24h;
-
-        var table_row = $("#table_row");
+        // Make a constant variable coinTable = to the id coinTable
+        const coinTable = $("#coinTable");
 
         // Make a function with the id and value as an argument
-        function cryptos(id, value) {
-
+        function cryptos() {
           // The loop starts at i and ends at the maximum length of data
-          for (var i = 0; i < data.length; i++) {
-            // Index+1
-            index++;
-            // Replace the html of the id to the value
-            $(id).html(value);
+          for (let i = 0; i < 100; i++) {
+
+            // Make a constant variable rank = to the market cap rank
+            const rank = data[i].market_cap_rank;
+
+            // Create a new img element
+            let logo = new Image();
+            // Set the sourse path
+            logo.src = data[i].image;
+
+            // Make a constant variable name = to the name
+            const name = data[i].name;
+            // Make a constant variable priceUsd = to the current price formated into USD
+            const priceUsd = new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            }).format(data[i].current_price);
+            // Make a constant variable marketCapPrice = to the market cap formated into USD
+            const marketCapPrice = new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            }).format(data[i].market_cap);
+            // Make a constant variable volumeUsd = to the total volume formated into USD
+            const volumeUsd = new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            }).format(data[i].total_volume);
+            // Make a constant variable circulatingSupply = to the circulating supply
+            const circulatingSupply = new Intl.NumberFormat().format(data[i].circulating_supply);
+            // Make a constant variable priceChange24 = to the 24h price change
+            const priceChange24 = data[i].price_change_24h;
+
+            // Append this to the coin table
+            coinTable.append(
+              // Append this to the table row
+              $("<tr></tr>").append(
+                // Replace the html of the table data to the rank
+                $("<td></td>").html(rank),
+                // Replace the html of the table data to the logo
+                $("<td></td>").html(logo),
+                // Replace the html of the table data to the name
+                $("<td></td>").html(name),
+                // Replace the html of the table data to the market cap price
+                $("<td></td>").html(marketCapPrice),
+                // Replace the html of the table data to the usd price
+                $("<td></td>").html(priceUsd),
+                // Replace the html of the table data to the usd volume
+                $("<td></td>").html(volumeUsd),
+                // Replace the html of the table data to the circulating supply
+                $("<td></td>").html(circulatingSupply),
+                // Replace the html of the table data to the 24h price change
+                $("<td></td>").html(priceChange24)
+              )
+            );
           }
         }
 
-        cryptos("#rank", rank);
-        cryptos("#name", name);
-        cryptos("#market_cap", market_cap);
-        cryptos("#price", price);
-        cryptos("#volume_24", volume_24);
-        cryptos("#circulating_supply", circulating_supply);
-        cryptos("#change_24", change_24);
-
-        console.log(data);
+        // Call function cryptos
+        cryptos();
 
       })
 
