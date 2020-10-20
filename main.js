47c5b06dbb8669a66198dac8359babaacc4f3987
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2020-10-20 14:29:33 +0200

    Add new logo, change color in js and change the style in css

--- main.js
+++ main.js
@@ -22,30 +22,33 @@ $(document).ready(function() {
       // Then make a finction with the data as the argument
       res.json().then(data => {
 
-        // The constant variable totalMarketCap = total market cap data in usd formated to usd
+        // Make a variable markets = data markets
+        var markets = data.data.markets;
+        // Make a variable activeCryptos = active cryptocurrencies
+        var activeCryptos = new Intl.NumberFormat().format(data.data.active_cryptocurrencies);
+        // Make a variable totalMarketCap = total market cap data in usd formated to usd
         var totalMarketCap = new Intl.NumberFormat('en-US', {
           style: 'currency',
           currency: 'USD'
         }).format(data.data.total_market_cap.usd);
-        // The constant variable markets = data markets
-        var markets = data.data.markets;
-        // The constant variable totalVolume = total volume data in usd formated to usd
+        // Make a variable totalVolume = total volume data in usd formated to usd
         var totalVolume = new Intl.NumberFormat('en-US', {
           style: 'currency',
           currency: 'USD'
         }).format(data.data.total_volume.usd);
-        // The constant variable btcDominance = market market cap percentage of bitcoin
+        // Make a variable btcDominance = market market cap percentage of bitcoin
         var btcDominance = data.data.market_cap_percentage.btc.toFixed(1);
 
-        // Replace the html of the id totalMarketCap with the total market cap
-        $("#totalMarketCap").html("Market Cap:" + " " + totalMarketCap);
-        // Replace the html of the id markets with the markets
-        $("#markets").html("Markets:" + " " + markets);
-        // Replace the html of the id totalVolume with the total volume
-        $("#totalVolume").html("Volume:" + " " + totalVolume);
-        // Replace the html of the id btcDominance with the market cap percentage of bitcoin
-        $("#btcDominance").html("BTC Dominance:" + " " + btcDominance + "%");
-
+        // Replace the html of the id marketsNumb with the markets
+        $("#marketsNumb").html(markets);
+        // Replace the html of the id activeCryptosNumb with the Active Cryptocurrencies
+        $("#activeCryptosNumb").html(activeCryptos)
+        // Replace the html of the id totalMarketCapNumb with the total market cap
+        $("#totalMarketCapNumb").html(totalMarketCap);
+        // Replace the html of the id totalVolumeNumb with the total volume
+        $("#totalVolumeNumb").html(totalVolume);
+        // Replace the html of the id btcDominanceNumb with the market cap percentage of bitcoin
+        $("#btcDominanceNumb").html(`${btcDominance} %`);
       })
     });
 
@@ -58,7 +61,7 @@ $(document).ready(function() {
       // Then make a function with the data as an argument
       res.json().then(data => {
 
-        // Make a constant variable coinTable = to the id coinTable
+        // Make a variable coinTable = to the id coinTable
         var coinTable = $("#coinTable");
 
         // Make a function with the id and value as an argument
@@ -66,7 +69,7 @@ $(document).ready(function() {
           // The loop starts at i and ends at the maximum length of data
           for (let i = 0; i < 100; i++) {
 
-            // Make a constant variable rank = to the market cap rank
+            // Make a variable rank = to the market cap rank
             var rank = data[i].market_cap_rank;
 
             // Create a new img element
@@ -74,26 +77,26 @@ $(document).ready(function() {
             // Set the sourse path
             logo.src = data[i].image;
 
-            // Make a constant variable name = to the name
+            // Make a variable name = to the name
             var name = data[i].name;
-            // Make a constant variable priceUsd = to the current price formated into USD
+            // Make a variable priceUsd = to the current price formated into USD
             var priceUsd = new Intl.NumberFormat('en-US', {
               style: 'currency',
               currency: 'USD'
             }).format(data[i].current_price);
-            // Make a constant variable marketCapPrice = to the market cap formated into USD
+            // Make a variable marketCapPrice = to the market cap formated into USD
             var marketCapPrice = new Intl.NumberFormat('en-US', {
               style: 'currency',
               currency: 'USD'
             }).format(data[i].market_cap);
-            // Make a constant variable volumeUsd = to the total volume formated into USD
+            // Make a variable volumeUsd = to the total volume formated into USD
             var volumeUsd = new Intl.NumberFormat('en-US', {
               style: 'currency',
               currency: 'USD'
             }).format(data[i].total_volume);
-            // Make a constant variable circulatingSupply = to the circulating supply
+            // Make a variable circulatingSupply = to the circulating supply
             var circulatingSupply = new Intl.NumberFormat().format(data[i].circulating_supply);
-            // Make a constant variable priceChange24 = to the 24h price change
+            // Make a variable priceChange24 = to the 24h price change
             var priceChange24 = data[i].price_change_percentage_24h_in_currency.toFixed(2);
 
             // Append this to the coin table
@@ -101,19 +104,19 @@ $(document).ready(function() {
               // Append this to the table row
               $("<tr></tr>").append(
                 // Replace the html of the table data to the rank
-                $("<td></td>").html(rank),
+                $("<th></th>").html(rank).css("color", "Black"),
                 // Replace the html of the table data to the logo
                 $("<td></td>").html(logo),
-                // Replace the html of the table data to the name
-                $("<td></td>").html(name),
-                // Replace the html of the table data to the market cap price
-                $("<td></td>").html(marketCapPrice),
-                // Replace the html of the table data to the usd price
-                $("<td></td>").html(priceUsd),
-                // Replace the html of the table data to the usd volume
-                $("<td></td>").html(volumeUsd),
-                // Replace the html of the table data to the circulating supply
-                $("<td></td>").html(circulatingSupply),
+                // Replace the html of the table data to the name with color orange
+                $("<td></td>").html(name).css("color", "#ff9d00"),
+                // Replace the html of the table data to the market cap price with color orange
+                $("<td></td>").html(marketCapPrice).css("color", "#ff9d00"),
+                // Replace the html of the table data to the usd price with color orange
+                $("<td></td>").html(priceUsd).css("color", "#ff9d00"),
+                // Replace the html of the table data to the usd volume with color orange
+                $("<td></td>").html(volumeUsd).css("color", "#ff9d00"),
+                // Replace the html of the table data to the circulating supply with color orange
+                $("<td></td>").html(circulatingSupply).css("color", "#ff9d00"),
                 // Call function color
                 color()
               )
@@ -125,11 +128,11 @@ $(document).ready(function() {
               // If the 24h price change < 0
               if(priceChange24 < 0) {
                 // Returns the data table with an id color replaced by the html to the 24h price change in red
-                return $("<td id=`#color`></td>").html(priceChange24).css("color", "red");
+                return $("<td id=`#color`></td>").html(`${priceChange24} %`).css("color", "#ff9696");
               }
               // else, returns the data table with an id color replaced by the html to the 24h price change in green
               else {
-                return $("<td id=`#color`></td>").html(priceChange24).css("color", "green");
+                return $("<td id=`#color`></td>").html(`${priceChange24} %`).css("color", "#78eb81");
               }
             }
           }
